3305a20bed3060ea39d05ee59d3383f94a37c379
anandkrish02 <42888735+anandkrish02@users.noreply.github.com>
2018-09-01 15:05:28 +0530

    Add files via upload

---
+++ httml/js.js
@@ -0,0 +1,14 @@
+function gets()
+{
+var x=document.GetElementById('lg').value;
+var y=document.GetElementById('lg1').value;
+var a="anand@gmail.com";
+var b="1234";
+if((a===to.String(x))&&(b===to.String(y)))
+{
+alert("password and username is valid");
+}
+else
+{
+alert("password and username is invalid");
+}
